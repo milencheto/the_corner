44e257879130a572b009768811436c84682d3813
Milena Ivanova <m.zh.ivanova90@gmail.com>
2017-07-26 17:22:55 +0300

    other files

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
+    livereload = require('gulp-livereload');
+
+
+//Starts server at localhost:2001
+ gulp.task('connect', function(){
+    connect.server({
+        port: 2001
+    });
+ });
+
+ gulp.task('html', function () {
+  gulp.src('*.html')
+    .pipe(livereload());
+});
+
+gulp.task('sassConvert', function () {
+    return gulp.src('assets/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.start(['connect']);
+  gulp.watch(['*.html'], ['html']);
+  gulp.watch(['assets/sass/*.scss', 'assets/css/*.css'], ['sassConvert']);
+});
+ 
